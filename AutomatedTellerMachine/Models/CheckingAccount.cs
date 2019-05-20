9839aadcfd9abb89f57d84aeb1365e95abe44056
rndev80 <rndev80@gmail.com>
2019-05-20 23:49:18 +0200

    Updated checking account model and detail page

--- AutomatedTellerMachine/Models/CheckingAccount.cs
+++ AutomatedTellerMachine/Models/CheckingAccount.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,9 +9,22 @@ namespace AutomatedTellerMachine.Models
     public class CheckingAccount
     {
         public int Id { get; set; }
+
+        [Display(Name = "Account #")]
         public string AccountNumber { get; set; }
+
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+
+        public string Name
+        {
+            get { return string.Format("{0} {1}", this.FirstName, this.LastName); }
+        }
+
+        [DataType(DataType.Currency)]
         public decimal Balance { get; set; }
     }
 }
